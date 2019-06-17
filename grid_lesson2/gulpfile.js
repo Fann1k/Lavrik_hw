c51272bebbf6009095ec093cb7188b48a5e90d60
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-06-17 17:03:48 +0300

    17/06/19

--- grid_lesson2/gulpfile.js
+++ grid_lesson2/gulpfile.js
@@ -6,22 +6,32 @@ const gulp = require('gulp'),
         css: {
             src: 'precss/**/*.css',
             dest: 'css'
-        }
+        },
+        html: '*.html'
     },
     sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    rename = require("gulp-rename");
 
 gulp.task('build', function (done) {
     gulp.src(config.root + config.css.src) //берем все подпапки в папке precss и все файлы с разрешением .css
 
         .pipe(sourcemaps.init())
+        .pipe(concat('styles.css'))
         .pipe(autoprefixer({
             overrideBrowserslist: ['> 0.1%'],
             cascade: false
         }))
-        .pipe(cleanCSS())
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.root + config.css.dest))
+
         .pipe(browserSync.reload({
             stream: true
         }));
@@ -30,6 +40,9 @@ gulp.task('build', function (done) {
 
 gulp.task('watch:styles', function () {
     gulp.watch(config.root + config.css.src, gulp.series('build')); //берет ./src/precss/**/*.css делает с ним 'build' + можно еще что-то после build добавить
+});
+gulp.task('watch:html', function () {
+    gulp.watch(config.root + config.html).on('change', browserSync.reload);
 });
 
 gulp.task('browser-sync', function () {
@@ -41,4 +54,4 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', gulp.series('build', gulp.parallel('watch:styles', 'browser-sync')));
+gulp.task('watch', gulp.series('build', gulp.parallel('watch:styles', 'watch:html', 'browser-sync')));
